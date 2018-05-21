e0ec82d36d79733c3d611a2755d04879539c552f
Serial Lab <slab@serial-lab.com>
2018-05-21 10:25:12 -0600

    re-added database settings for non-local testing.

--- tests/settings.py
+++ tests/settings.py
@@ -10,12 +10,12 @@ USE_TZ = True
 SECRET_KEY = "ffry&l%(2g&4^+&7e9nu0+@bpr!j-%0_zz3gn-l3bt)&k4=95r"
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": ":memory:",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
 
 ROOT_URLCONF = "tests.urls"
 
