273cc6213fc15ced274e649fac45532025fc4c00
Serial Lab <slab@serial-lab.com>
2018-05-23 13:44:35 -0600

    Update for test data.

--- quartet_masterdata/management/commands/md_test_data.py
+++ quartet_masterdata/management/commands/md_test_data.py
@@ -71,9 +71,10 @@ class LocationTypeFactory(factory.django.DjangoModelFactory):
 class LocationFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Location
-    GLN13 = '3055551232569'
-    SGLN = 'urn:epc:id:sgln:305555.123456.8'
-    name = "Factory"
+
+    GLN13 = '3055551234561'
+    SGLN = 'urn:epc:id:sgln:305555.123456.0'
+    name = "Headquarters"
     address1 = 'One Citizens Bank Way'
     country = 'US'
     city = 'Philadelphia'
@@ -81,7 +82,7 @@ class LocationFactory(factory.django.DjangoModelFactory):
     postal_code = '19148'
     latitude = '39.906098'
     longitude = '-75.165733'
-    location_type = factory.Iterator(models.LocationType.objects.all())
+    location_type = factory.SubFactory(LocationTypeFactory)
 
 
 class LocationFieldFactory(factory.django.DjangoModelFactory):
@@ -91,7 +92,7 @@ class LocationFieldFactory(factory.django.DjangoModelFactory):
     name = 'Internal Code'
     value = 'PL72'
     description = 'Internal plant code #72.'
-    location = factory.Iterator(models.Location.objects.all())
+    location = factory.SubFactory(LocationFactory)
 
 
 class LocationIdentifierFactory(factory.django.DjangoModelFactory):
@@ -101,14 +102,24 @@ class LocationIdentifierFactory(factory.django.DjangoModelFactory):
     identifier = 'urn:epc:id:sgln:305555.123456.1'
     identifier_type = 'SGLN'
     description = 'First Base'
-    location = factory.Iterator(models.Location.objects.all())
+    location = factory.SubFactory(LocationFactory)
+
+class CompanyTypeFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.CompanyType
+
+    identifier = 'MANU'
+    description = 'Manufacturer'
 
 class CompanyFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Company
-    GLN13 = '3055551232569'
-    SGLN = 'urn:epc:id:sgln:305555.123456.8'
-    name = "Factory"
+    SGLN = 'urn:epc:id:sgln:305555.123456.0'
+    gs1_company_prefix = '234123'
+    company_type = factory.SubFactory(CompanyTypeFactory)
+    GLN13 = '3055551234561'
+    SGLN = 'urn:epc:id:sgln:305555.123456.0'
+    name = "Headquarters"
     address1 = 'One Citizens Bank Way'
     country = 'US'
     city = 'Philadelphia'
@@ -116,20 +127,12 @@ class CompanyFactory(factory.django.DjangoModelFactory):
     postal_code = '19148'
     latitude = '39.906098'
     longitude = '-75.165733'
-    location_type = factory.Iterator(models.LocationType.objects.all())
-    company_type = factory.Iterator(models.CompanyType.objects.all())
 
-class CompanyTypeFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = models.CompanyType
-
-    identifier = 'MANU'
-    description = 'Manufacturer'
 
 class TradeItemFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.TradeItem
-    company = factory.Iterator(models.Company.objects.all())
+    company = factory.SubFactory(CompanyFactory)
     country_of_origin = 'US'
     drained_weight = None
     gross_weight = 10.5
@@ -156,7 +159,7 @@ class TradeItemFieldFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.TradeItemField
 
-    trade_item = factory.Iterator(models.TradeItem.objects.all())
+    trade_item = factory.SubFactory(TradeItemFactory)
     name = 'MATNO'
     value = '32423-33-333'
     description = 'SAP Internal Material Number'
