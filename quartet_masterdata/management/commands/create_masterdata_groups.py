35006d8ff696891c36ba44bf0d874a131849728f
Serial Lab <slab@serial-lab.com>
2018-08-01 12:33:45 -0600

    Fixed error in management command.

--- quartet_masterdata/management/commands/create_masterdata_groups.py
+++ quartet_masterdata/management/commands/create_masterdata_groups.py
@@ -15,10 +15,7 @@
 
 from django.contrib.auth.models import Permission, Group
 from django.db.models import Q
-from quartet_masterdata import models
-from django.db import utils
-import factory
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 
 
